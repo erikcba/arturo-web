6f017ee0fbc6d112d33c7395a6211438912844ac
Erik Tomaselli <eriktomaselli97@gmail.com>
2025-10-13 19:05:42 -0300

    agregado de menu con animaciones

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,16 +7,26 @@ const Menu = ({ className }) => {
             <div className='container h-full mx-auto flex flex-row justify-center items-center xl:gap-14 gap-8 z-20'>
                 <div className='flex flex-col items-center justify-center gap-4 ml-auto'>
                     <Link to='/'>
-                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none'><span className='text-white tracking-widest text-4xl font-bold'>01/</span> INDEX</p>
+                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none group  hover:bg-white transition-all duration-300 ease-in-out px-24 hover:text-black'>
+                            <span className='text-white tracking-widest text-4xl font-bold group-hover:text-black'>01/</span>
+                            INDEX</p>
                     </Link>
                     <Link to='/about'>
-                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none'>ABOUT <span className='text-white tracking-widest text-4xl font-bold'>/02</span></p>
+                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none group hover:bg-white transition-all duration-300 ease-in-out px-24 hover:text-black'>
+                            ABOUT
+                            <span className='text-white tracking-widest text-4xl font-bold group-hover:text-black'>/02</span>
+                        </p>
                     </Link>
                     <Link to='/calendar'>
-                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none'><span className='text-white tracking-widest text-4xl font-bold'>03/</span> CALENDAR</p>
+                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none group hover:bg-white transition-all duration-300 ease-in-out px-12 hover:text-black'>
+                            <span className='text-white tracking-widest text-4xl font-bold group-hover:text-black'>03/</span>
+                            CALENDAR</p>
                     </Link>
                     <Link to='/contact'>
-                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none'>CONTACT <span className='text-white tracking-widest text-4xl font-bold'>/04</span></p>
+                        <p className='text-white text-8-5 font-xxlight text-lexend tracking-max leading-none group hover:bg-white transition-all duration-300 ease-in-out px-12 hover:text-black'>
+                            CONTACT
+                            <span className='text-white tracking-widest text-4xl font-bold group-hover:text-black'>/04</span>
+                        </p>
                     </Link>
                 </div>
                 <div className='flex flex-col justify-center items-center gap-4 '>
