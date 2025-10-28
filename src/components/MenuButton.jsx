bc065270171d93c6dd80f1f59f018428062920ab
Erik Tomaselli <eriktomaselli97@gmail.com>
2025-10-28 18:11:49 -0300

    agregado de animaciones, falta de contacto

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -3,11 +3,11 @@ export default function MenuButton({ isOpen, onClick, bgColor = "bg-black" }) {
   return (
     <button
       onClick={onClick}
-      className={`menu-btn ml-auto sm:mr-auto  ${isOpen ? "active" : ""}`}
+      className={`menu-btn ml-auto sm:mr-auto group  ${isOpen ? "active " : ""}`}
     >
+      <span className={`line ${bgColor} ${isOpen ? "bg-white group-hover:rotate-90" : ""}`}></span>
       <span className={`line ${bgColor} ${isOpen ? "bg-white" : ""}`}></span>
-      <span className={`line ${bgColor} ${isOpen ? "bg-white" : ""}`}></span>
-      <span className={`line ${bgColor} ${isOpen ? "bg-white" : ""}`}></span>
+      <span className={`line ${bgColor} ${isOpen ? "bg-white group-hover:rotate-90" : ""}`}></span>
     </button>
   );
 }
